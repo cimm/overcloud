6f53e422a2f98131460a5454e9b01555564190bc
Simon Schoeters <b2282e9d-46e8-4ec4-946a-b6ad84078b1c@astil.be>
2025-03-12 22:53:29 +0100

    Run directly from nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "OverCloud Server Development Environment";
+  description = "OverCloud, an Overland backend";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
@@ -18,6 +18,18 @@
 
       in
       with pkgs; {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "run-overcloud";
+          src = ./.;
+          buildInputs = [ pythonEnv ];
+          buildPhase = "python main.py";
+          installPhase = "mkdir -p $out/bin && cp $buildScript $out/bin/run-overcloud";
+          buildScript = pkgs.writeShellScript "run-overcloud-script" ''
+            #!${pythonEnv}/bin/python
+            python main.py
+          '';
+        };
+
         devShells.default = mkShell {
           buildInputs = [
             pythonEnv
@@ -29,3 +41,34 @@
       }
     );
 }
+#{
+#  description = "OverCloud, an Overland backend";
+#
+#  inputs = {
+#    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+#    flake-utils.url = "github:numtide/flake-utils";
+#  };
+#
+#  outputs = { self, nixpkgs, flake-utils }:
+#    flake-utils.lib.eachDefaultSystem (system:
+#      let
+#        pkgs = nixpkgs.legacyPackages.${system};
+#
+#        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+#          geopandas
+#          shapely
+#        ]);
+#
+#      in
+#      with pkgs; {
+#        devShells.default = mkShell {
+#          buildInputs = [
+#            pythonEnv
+#            yapf
+#          ];
+#        };
+#
+#        formatter = pkgs.nixpkgs-fmt;
+#      }
+#    );
+#}
