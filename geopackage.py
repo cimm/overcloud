4acd09152c191d188eab28f6ae5497aef048959c
Simon Schoeters <b2282e9d-46e8-4ec4-946a-b6ad84078b1c@astil.be>
2025-03-13 17:05:32 +0100

    Use template for GeoDataFrame columns

--- geopackage.py
+++ geopackage.py
@@ -6,36 +6,53 @@ import pandas as pd
 class GeoPackage:
     driver = 'GPKG'
     crs = 'EPSG:4326'
+    template = {
+        "geometry": [None],
+        "altitude": [None],
+        "battery_level": [None],
+        "battery_state": [None],
+        "course": [None],
+        "course_accuracy": [None],
+        "horizontal_accuracy": [None],
+        "motion": [],
+        "speed": [None],
+        "speed_accuracy": [None],
+        "timestamp":  [None],
+        "trip_id": [None],
+        "vertical_accuracy": [None],
+        "wifi": [None]
+    }
 
     def __init__(self, path, layer):
         self.path = path
         self.layer = layer
 
     def json_to_frame(self, json):
-        data = {}
+        data = self.template
         data['geometry'] = [Point(json['geometry']['coordinates'])]
         for key, value in json['properties'].items():
             if value is None:
-                data[key] = [None]
                 continue
             match key:
-                case 'timestamp':
-                    value = pd.to_datetime(value)
                 case 'altitude':
                     value = int(value)
-                case 'speed':
+                case 'battery_level':
                     value = int(value)
                 case 'course':
                     value = int(value)
+                case 'course_accuracy':
+                    value = int(value)
                 case 'horizontal_accuracy':
                     value = int(value)
-                case 'vertical_accuracy':
+                case 'speed':
                     value = int(value)
                 case 'speed_accuracy':
                     value = int(value)
-                case 'course_accuracy':
-                    value = int(value)
-                case 'battery_level':
+                case 'timestamp':
+                    value = pd.to_datetime(value)
+                case 'trip_id':
+                    value = pd.to_datetime(value)
+                case 'vertical_accuracy':
                     value = int(value)
             data[key] = [value]
         return gpd.GeoDataFrame(data, crs=self.crs)
